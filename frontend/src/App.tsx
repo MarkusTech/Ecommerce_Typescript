0d9346e6780bee7f981ee41948fdfda0fcef0196
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-09-18 13:58:44 +0800

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { sampleProducts } from './data';
 
 function App() {
   return (
@@ -7,7 +8,17 @@ function App() {
         WMR.ORG
       </header>
       <main>
-        body
+        <ul>
+          {
+            sampleProducts.map(product => (
+              <li key={product.slug}>
+                <img src={product.image} alt={product.name} className='product-image'/>
+                <h2>{product.name}</h2>
+                <p>PHP {product.price}</p>
+              </li>
+            ))
+          }
+        </ul>
       </main>
       <footer>
         All right reserved!
---
+++ frontend/src/data.ts
@@ -0,0 +1,56 @@
+import { Product } from "./types/Product";
+
+export const sampleProducts: Product[] = [
+  {
+    name: "Nike Slim shirt",
+    slug: "nike-slim-shirt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlCsi8HNFWyIiCVyonv-wftPRUqtQ6YzYtWvWWIp4NbQ&s",
+    category: "Shirts",
+    brand: "Nike",
+    price: 120,
+    countInStock: 10,
+    description: "High Quality Shirt",
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: "Adidas Shirt",
+    slug: "adidas shirt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1LtiipQl_vrfVhNQaO9rmHwybMCWDLAWW5DqCVSDXhA&s",
+    category: "Shirts",
+    brand: "Addidas",
+    price: 180,
+    countInStock: 10,
+    description: "High Quality Shirt",
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: "Lee Slim shirt",
+    slug: "lee-slim-shirt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr7bJxaAHes9oj3TjpXpfOamfuA5tQWxjXnD2Ffe4V7w&s",
+    category: "Shirts",
+    brand: "Lee",
+    price: 150,
+    countInStock: 10,
+    description: "High Quality Shirt",
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: "Lacoste Slim shirt",
+    slug: "lacoste-slim-shirt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdq_NeQVz7eVnHysGgmZ3lCjRPNFGyi_gC4B4NOQKDaA&s",
+    category: "Shirts",
+    brand: "Lacoste",
+    price: 200,
+    countInStock: 10,
+    description: "High Quality Shirt",
+    rating: 4.5,
+    numReviews: 10,
+  },
+];
